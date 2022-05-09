6968b53c9cf56b3378d0eb57f329dc2f3910dfb5
Subhash Yadav <99490164+subhashydv@users.noreply.github.com>
2022-05-09 15:37:16 +0530

    Added html generator

---
+++ generateHtml.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+const generateTag = function (tag, description, tagClass) {
+  const classTag = tagClass ? ' class="' + tagClass + '"' : '';
+  return '<' + tag + classTag + '>' + description + '</' + tag + '>';
+};
+
+const generateRow = function (content) {
+  const headers = Object.keys(content);
+  const row = headers.map((header) => generateTag('td', content[header]));
+  return generateTag('tr', row.join(''));
+};
+
+const generateHeader = function (content) {
+  const headers = Object.keys(content);
+  return headers.map((header) => generateTag('th', header)).join('');
+};
+
+const generateTable = function (content) {
+  const header = generateHeader(content[0]);
+  const rows = content.map((rowContent) => generateRow(rowContent));
+  const table = generateTag('tr', header) + rows.join('');
+  return generateTag('table', table);
+};
+
+const generateLink = (href = 'style.css', rel = 'stylesheet') => {
+  return '<link rel="' + rel + '" href=' + href + '>';
+};
+
+const generateHead = function (title, href, rel) {
+  const headContent = generateTag('title', title) + generateLink(rel, href);
+  return generateTag('head', headContent);
+};
+
+const generateBody = function (content) {
+  return generateTag('body', generateTable(content))
+};
+
+const generateHtml = function (content, title) {
+  return generateTag('html', generateHead(title) + generateBody(content));
+};
+
+
+const content = JSON.parse(fs.readFileSync('./horseData.json', 'utf8'));
+
+output = generateHtml(content, 'Dream12');
+fs.writeFileSync('horse.html', output, 'utf8');
