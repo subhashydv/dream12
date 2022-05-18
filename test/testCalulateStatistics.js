ca6c2988748c60c01fb55809d4140a1944f81533
Subhash Yadav <99490164+subhashydv@users.noreply.github.com>
2022-05-18 12:58:43 +0530

    Added test for function 'chanceToWin' in calculateStatistics

--- test/testCalulateStatistics.js
+++ test/testCalulateStatistics.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { probabilityToWin, percentOfWinning } = require('../src/calculateStatistics');
+const { probabilityToWin, percentOfWinning, chanceToWin } = require('../src/calculateStatistics');
 
 describe('probabilityToWin', () => {
   it('Should return list of probability for single horse', () => {
@@ -10,6 +10,7 @@ describe('probabilityToWin', () => {
     }];
     assert.deepStrictEqual(probabilityToWin(data), [0.5]);
   });
+
   it('Should return list of probability for multiple horse', () => {
     const data = [{
       name: 'Chetak',
@@ -31,5 +32,50 @@ describe('percentOfWinning', () => {
   });
   it('Should return relative winning chance (multiple horse)', () => {
     assert.deepStrictEqual(percentOfWinning([0.5, 0.25]), [66.67, 33.33]);
+  });
+});
+
+describe('chanceToWin', () => {
+  it('Should return object with winning chance', () => {
+    const data = [{
+      name: 'Lilly',
+      racesRan: 20,
+      racesWon: 10
+    }];
+
+    const expected = [{
+      name: 'Lilly',
+      racesRan: 20,
+      racesWon: 10,
+      chance: 100
+    }];
+    assert.deepStrictEqual(chanceToWin(data), expected);
+  });
+
+  it('Should return list of object with winning chance', () => {
+    const data = [{
+      name: 'Lilly',
+      racesRan: 20,
+      racesWon: 10
+    },
+    {
+      name: 'Ranger',
+      racesRan: 30,
+      racesWon: 10
+    }];
+
+    const expected = [{
+      name: 'Lilly',
+      racesRan: 20,
+      racesWon: 10,
+      chance: 60
+    },
+    {
+      name: 'Ranger',
+      racesRan: 30,
+      racesWon: 10,
+      chance: 40
+    }];
+    assert.deepStrictEqual(chanceToWin(data), expected);
   });
 });
